fbc4fac2eff54e380a47b94e937c6c8412b49cb3
Stefan Djikic <stefandjikic@outlook.com>
2022-11-29 21:09:58 +0100

    add favicon and icons

--- pages/index.js
+++ pages/index.js
@@ -104,6 +104,28 @@ export default function Home({ scoreResults = [] }) {
         <title>FIFA World Cup 2022 Competition</title>
         <meta name="description" content="FIFA World Cup Quatar competition" />
         <link rel="icon" href="/favicon.ico" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#550265" />
       </Head>
       <Nav />
       {loading ||
